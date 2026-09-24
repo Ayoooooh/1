40f14350af42e3c4a4fb734ce9bdae8d6c6354e0
Ayoooooh <31814445+Ayoooooh@users.noreply.github.com>
2026-09-24 10:29:53 +0800

    build: add app/src/main/java/com/personal/savings/MainActivity.java

---
+++ app/src/main/java/com/personal/savings/MainActivity.java
@@ -0,0 +1,38 @@
+package com.personal.savings;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.graphics.Color;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(21, 29, 56));
+        getWindow().setNavigationBarColor(Color.rgb(245, 247, 251));
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
